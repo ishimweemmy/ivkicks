eea6364f3d81cd07f8c8b7fd7b75fb8131010fc1
Ishimwe-emma <ishimweemmanuel2005@gmail.com>
2022-12-20 22:06:52 +0200

    [feat]: completed the diffCard section

--- src/components/promo-products/DiffCard.tsx
+++ src/components/promo-products/DiffCard.tsx
@@ -8,10 +8,10 @@ interface DiffProps {
 const DiffCard: FC<DiffProps> = (props) => {
     const { heading, content, imgUrl } = props;
     return (
-        <div className="w-full h-fit bg-white flex flex-col items-center justify-center gap-[1rem] p-[2rem] hover:bg-[#315bff] border ">
+        <div className="w-full h-fit bg-white flex flex-col items-center justify-center gap-[1rem] p-[2rem] hover:bg-[#315bff] border hover:shadow-2xl group transition-all duration-150">
             <img src={imgUrl} alt="" className="w-16 h-16" />
-            <span>{heading}</span>
-            <p className="text-center">{content}</p>
+            <span className="text-center text-lg font-bold group-hover:text-white">{heading}</span>
+            <p className="text-center text-sm group-hover:text-white">{content}</p>
         </div>
     )
 }
