3b17f9f7c21d7a6d6543f20b51c033c92e4f9ea8
ishimwe-emma <ishimweemmanuel2005@gmail.com>
2023-01-23 19:47:18 +0200

    [feat]: completed the collection component

---
+++ src/components/Products/Collection.tsx
@@ -0,0 +1,30 @@
+import { FC, memo } from "react";
+
+interface CollectionProps {
+  id: number;
+  imgSrc: string;
+  categoryName: string;
+  productsNbr: number;
+}
+
+const Collection: FC<CollectionProps> = (props) => {
+  const { id, imgSrc, categoryName, productsNbr } = props;
+  return (
+    <div
+      className="w-full h-full bg-cover bg-no-repeat flex flex-col items-center justify-end pb-2"
+      style={{
+        backgroundImage: `url(${imgSrc})`,
+        backgroundPosition: `center`,
+      }}
+    >
+      <span className="text-[rgb(10,8,58)] text-xl font-bold 2xl:text-3xl">
+        {categoryName}
+      </span>
+      <p className="text-sm text-white text-center tracking-wider px-8 lsm:text-start font-semibold">
+        {productsNbr} products
+      </p>
+    </div>
+  );
+};
+
+export default memo(Collection);
