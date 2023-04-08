ca172a70f669e472777386f8e6716e0703b14da4
ishimwe-emma <ishimweemmanuel2005@gmail.com>
2023-04-09 00:34:34 +0200

    [mod]: modified the store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userReducer";
-import { RestaurantMenuSharp } from "@mui/icons-material";
 
 export const store = configureStore({
   reducer: {
